51998703d3849c117326588a763c54799b3a643c
ltisoy <danieltisoy2001@gmail.com>
2021-11-13 12:33:28 -0500

    graphql hello wold
    using graphql with express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema"
+
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello World",
+  });
+});
+
+
+
+app.use("/graphql", graphqlHTTP({
+    graphiql:true,
+    schema: schema
+}));
+
 app.listen(3000, () => console.log("server on port 3000"));
---
+++ src/schema.js
@@ -0,0 +1,15 @@
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { resolvers } from "./resolvers";
+
+const typeDefs = `
+
+    type Query{
+        hello: String
+    }
+
+`;
+
+export default makeExecutableSchema({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
