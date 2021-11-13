3db53d45e5ffa613813d775bc1d61bc2afc2dcfc
ltisoy <danieltisoy2001@gmail.com>
2021-11-13 12:10:37 -0500

    configuracion de babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const express = require("express");
-
+import express from "express";
 const app = express();
-app.listen(3000, ()=>console.log("server on port 3000"))
+app.listen(3000, () => console.log("server on port 3000"));
