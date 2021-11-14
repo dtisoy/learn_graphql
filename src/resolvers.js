8dcd5f38a39f553fe700d3540b0a7b72c22108a5
ltisoy <danieltisoy2001@gmail.com>
2021-11-14 12:51:33 -0500

    basic users crud

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,19 +1,35 @@
 import { tasks } from "./sample";
+import User from "./models/User";
 
 export const resolvers = {
   Query: {
     hello: () => "hello world with graphql",
     greet: (root, { name }, context) => {
-        console.log(context)
+      console.log(context);
       return `Hello ${name} :)`;
     },
     tasks: () => tasks,
+    users: async () => {
+      return await User.find();
+    },
   },
   Mutation: {
     createTask: (_, { input }) => {
       input._id = tasks.length;
       tasks.push(input);
       return input;
+    },
+    createUser: async (_, { input }) => {
+      const newUser = new User(input);
+      await newUser.save();
+      return newUser;
+    },
+    deleteUser: async (_, { _id }) => {
+      return await User.findByIdAndDelete(_id);
+    },
+    updateUser: async (_, { _id , input}) => {
+      // the function return the old user, that's why the third parameter
+      return await User.findByIdAndUpdate(_id, input, { new: true });
     },
   },
 };
--- src/schema.js
+++ src/schema.js
@@ -7,6 +7,7 @@ const typeDefs = `
         hello: String
         greet(name: String!): String
         tasks: [Task]
+        users: [User]
     }
 
     type Task{
@@ -16,14 +17,30 @@ const typeDefs = `
        number: Int               
     }
 
+    type User{
+        _id: ID
+        firstname: String!
+        lastname: String!
+        age: Int!
+    }
+
     type Mutation{
         createTask(input: TaskInput): Task
+        createUser(input: UserInput ): User
+        deleteUser(_id: ID): User
+        updateUser(_id: ID, input: UserInput): User
     }
 
     input TaskInput{
        title: String!
        description: String!
        number: Int
+    }
+
+    input UserInput{
+        firstname: String!
+        lastname: String!
+        age: Int
     }
 
 `;
